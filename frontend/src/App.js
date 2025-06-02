1a2dad64d59c0a87bc8801e96c6753160023c393
Ragavendran Ramalingam <rramalingam@cloudbees.com>
2025-06-02 11:04:35 +0530

    RAG changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,13 @@ import React from "react";
 import ChatBox from "./components/ChatBox";
 
 function App() {
-    const authUrl = `https://auth.atlassian.com/authorize?audience=api.atlassian.com&client_id=${CLIENT_ID}&scope=read:confluence-content.all&redirect_uri=${REDIRECT_URI}&response_type=code&prompt=consent`;
+  const clientId = "your_client_id_here";
+  const redirectUri = "your_redirect_uri_here";
+  const authUrl = `https://auth.atlassian.com/authorize?audience=api.atlassian.com&client_id=${clientId}&scope=read:confluence-content.all&redirect_uri=${redirectUri}&response_type=code&prompt=consent`;
+  //  const authUrl = `https://auth.atlassian.com/authorize?audience=api.atlassian.com&client_id=${CLIENT_ID}&scope=read:confluence-content.all&redirect_uri=${REDIRECT_URI}&response_type=code&prompt=consent`;
 
-window.location.href = authUrl;
+  window.location.href = authUrl;
+//window.location.href = authUrl;
 
   return (
     <div>
